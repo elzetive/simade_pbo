ca3b583c18f884d5f97a69bab184588d93b2cbf4
elzetive <dimasriyan1791@gmail.com>
2026-07-07 16:55:25 +0700

    fix bikin aplikasi

--- simade_pbo/FormLogin.Designer.cs
+++ simade_pbo/FormLogin.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -50,9 +51,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
-            this.label2.Location = new System.Drawing.Point(543, 89);
+            this.label2.Location = new System.Drawing.Point(724, 110);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(267, 45);
+            this.label2.Size = new System.Drawing.Size(338, 54);
             this.label2.TabIndex = 2;
             this.label2.Text = "Selamat Datang!";
             // 
@@ -61,9 +63,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(114)))), ((int)(((byte)(128)))));
-            this.label3.Location = new System.Drawing.Point(547, 134);
+            this.label3.Location = new System.Drawing.Point(729, 165);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(241, 21);
+            this.label3.Size = new System.Drawing.Size(301, 28);
             this.label3.TabIndex = 3;
             this.label3.Text = "Silakan login menggunakan akun";
             // 
@@ -72,9 +75,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
-            this.label4.Location = new System.Drawing.Point(547, 195);
+            this.label4.Location = new System.Drawing.Point(729, 240);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(125, 20);
+            this.label4.Size = new System.Drawing.Size(156, 25);
             this.label4.TabIndex = 4;
             this.label4.Text = "Username/Email";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -84,28 +88,31 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
-            this.label5.Location = new System.Drawing.Point(547, 275);
+            this.label5.Location = new System.Drawing.Point(729, 338);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(76, 20);
+            this.label5.Size = new System.Drawing.Size(97, 25);
             this.label5.TabIndex = 5;
             this.label5.Text = "Password";
             // 
             // txtUsername
             // 
             this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsername.Location = new System.Drawing.Point(551, 218);
+            this.txtUsername.Location = new System.Drawing.Point(735, 268);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(435, 29);
+            this.txtUsername.Size = new System.Drawing.Size(579, 34);
             this.txtUsername.TabIndex = 7;
             this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
             // 
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(551, 298);
+            this.txtPassword.Location = new System.Drawing.Point(735, 367);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '●';
-            this.txtPassword.Size = new System.Drawing.Size(435, 29);
+            this.txtPassword.Size = new System.Drawing.Size(579, 34);
             this.txtPassword.TabIndex = 8;
             this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
             // 
@@ -116,9 +123,10 @@
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(551, 362);
+            this.btnLogin.Location = new System.Drawing.Point(735, 446);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(435, 39);
+            this.btnLogin.Size = new System.Drawing.Size(580, 48);
             this.btnLogin.TabIndex = 9;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = false;
@@ -132,8 +140,9 @@
             this.backgroud.Controls.Add(this.overlay);
             this.backgroud.Dock = System.Windows.Forms.DockStyle.Left;
             this.backgroud.Location = new System.Drawing.Point(0, 0);
+            this.backgroud.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.backgroud.Name = "backgroud";
-            this.backgroud.Size = new System.Drawing.Size(492, 588);
+            this.backgroud.Size = new System.Drawing.Size(656, 724);
             this.backgroud.TabIndex = 0;
             // 
             // overlay
@@ -143,8 +152,9 @@
             this.overlay.Controls.Add(this.judul);
             this.overlay.Dock = System.Windows.Forms.DockStyle.Fill;
             this.overlay.Location = new System.Drawing.Point(0, 0);
+            this.overlay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.overlay.Name = "overlay";
-            this.overlay.Size = new System.Drawing.Size(492, 588);
+            this.overlay.Size = new System.Drawing.Size(656, 724);
             this.overlay.TabIndex = 1;
             // 
             // label1
@@ -152,9 +162,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label1.Location = new System.Drawing.Point(70, 261);
+            this.label1.Location = new System.Drawing.Point(93, 321);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(359, 25);
+            this.label1.Size = new System.Drawing.Size(466, 32);
             this.label1.TabIndex = 1;
             this.label1.Text = "Sistem Informasi Manajemen Aset Desa";
             // 
@@ -163,9 +174,10 @@
             this.judul.AutoSize = true;
             this.judul.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.judul.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.judul.Location = new System.Drawing.Point(165, 204);
+            this.judul.Location = new System.Drawing.Point(220, 251);
+            this.judul.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.judul.Name = "judul";
-            this.judul.Size = new System.Drawing.Size(163, 50);
+            this.judul.Size = new System.Drawing.Size(206, 62);
             this.judul.TabIndex = 0;
             this.judul.Text = "SIMADE";
             this.judul.Click += new System.EventHandler(this.judul_Click);
@@ -176,10 +188,10 @@
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(1003, 10);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(1337, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(34, 24);
+            this.button1.Size = new System.Drawing.Size(45, 30);
             this.button1.TabIndex = 10;
             this.button1.Text = "X";
             this.button1.UseVisualStyleBackColor = false;
@@ -191,20 +203,19 @@
             this.lblKeRegister.Cursor = System.Windows.Forms.Cursors.Hand;
             this.lblKeRegister.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblKeRegister.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(130)))), ((int)(((byte)(246)))));
-            this.lblKeRegister.Location = new System.Drawing.Point(681, 417);
-            this.lblKeRegister.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblKeRegister.Location = new System.Drawing.Point(908, 513);
             this.lblKeRegister.Name = "lblKeRegister";
-            this.lblKeRegister.Size = new System.Drawing.Size(171, 19);
+            this.lblKeRegister.Size = new System.Drawing.Size(212, 23);
             this.lblKeRegister.TabIndex = 11;
             this.lblKeRegister.Text = "Belum punya akun? Daftar";
             this.lblKeRegister.Click += new System.EventHandler(this.lblKeRegister_Click);
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
-            this.ClientSize = new System.Drawing.Size(1046, 588);
+            this.ClientSize = new System.Drawing.Size(1395, 724);
             this.Controls.Add(this.lblKeRegister);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnLogin);
@@ -216,6 +227,8 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.backgroud);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login SIMADE";
